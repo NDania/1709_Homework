9f6cf8b9646f7d314846bb99b8cfef46879c9d80
Dania <Dania2008@list.ru>
2022-09-20 17:28:39 +0300

    Added tasks 2, 4, 6, 8

---
+++ Task 2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее
+Console.WriteLine("Введите первое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if(n < m)
+{
+      Console.WriteLine("max: " + m);
+      Console.WriteLine("min: " + n);
+}
+else 
+{
+      Console.WriteLine("max: " + n);   
+      Console.WriteLine("min: " + m);      
+}
---
+++ Task 6/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая на вход принимает число и выдает является ли число четным (делится ли оно на два без остатка) 
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if(n % 2 == 0)
+    Console.WriteLine("Число является четным");
+else       
+    Console.WriteLine("Число является нечетным");
